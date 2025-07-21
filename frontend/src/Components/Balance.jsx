f403483a8f3dd769b8a78c6ed29fadf47d2f04e4
ishrat2004 <ishratalikhan004@gmail.com>
2025-07-21 19:12:19 +0530

    small issue with balance resolved

--- frontend/src/Components/Balance.jsx
+++ frontend/src/Components/Balance.jsx
@@ -6,7 +6,7 @@ export function Balance(){
     const [showbalance , setbalance]=useState(false); 
     const [amount,set_amount]=useState(0);
     async function getbalance(){ 
-         const response=await axios.get("http://localhost:3000/api/v1/account/balance",{ 
+         const response=await axios.get("https://payufriend.onrender.com/api/v1/account/balance",{ 
                  headers:{ 
                     Authorization:`Bearer ${localStorage.getItem("token")}`
                  }
